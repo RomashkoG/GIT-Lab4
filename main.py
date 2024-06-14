c312170c581ca1117a071355a8f5da06fa225b41
RomashkoG <romashko.g.r@knu.ua>
2024-06-14 16:23:52 +0300

    create main.py, utils.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils import factorial, is_prime
+
+if __name__ == "__main__":
+    number = 5
+    print(f"Факторіал числа {number} = {factorial(number)}")
---
+++ utils.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
